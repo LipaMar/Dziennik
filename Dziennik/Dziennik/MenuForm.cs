b47c48427f59449222e5521b8946bbe18d608b37
LipaMar <48134943+LipaMar@users.noreply.github.com>
2019-06-16 16:50:42 +0200

    timeTable + database

--- Dziennik/Dziennik/MenuForm.cs
+++ Dziennik/Dziennik/MenuForm.cs
@@ -15,33 +15,29 @@ namespace Dziennik
         User user;
         List<Grade> grades;
         List<Class> classes;
-        int todayDay;
-        int todayMonth;
         DateTime calendar;
-        public enum Date
-        {
-            DAY,WEEK,MONTH
-        }
+       
         public MenuForm(string index)
         {
             user = new User(index);
             InitializeComponent();
-            todayDay = DateTime.Today.Day;
-            todayMonth = DateTime.Today.Month;
+            grades = user.GetGrades();
+            classes = user.GetClasses();
             calendar = DateTime.Today;
-            calendar = calendar.AddMonths(-1);
 
             infoLabel.Text = String.Format("{0} {1} ({2})", user.GetName(), user.GetLastName(), user.GetIndex());
             gradesPanel.Visible = false;
             timetablePanel.Visible = false;
             initGradesTable();
         }
+        private enum Date
+        {
+            DAY, WEEK, MONTH
+        }
         private void initGradesTable()
         {
             float sum = 0;
-            int nrOfSubjects = 0;
-            grades = user.GetGrades();
-            classes = user.GetClasses();
+            int nrOfSubjects = 0; 
             foreach (Grade x in grades)
             {
                 ListViewItem item = new ListViewItem();
@@ -55,7 +51,7 @@ namespace Dziennik
                     nrOfSubjects++;
                 }
                 gradesListView.Items.Add(item);
-                averageLabel.Text = String.Format("Średnia arytmetczna {0,-10:f2}", sum / nrOfSubjects);
+                averageLabel.Text = String.Format("Średnia arytmetczna {0,0:f2}", sum / nrOfSubjects);
             }
         }
 
@@ -98,19 +94,19 @@ namespace Dziennik
 
         private void dayRadio_CheckedChanged(object sender, EventArgs e)
         {
-           // calendar = DateTime.Today;
+            calendar = DateTime.Today;
             SetTimeSpan(Date.DAY);
         }
 
         private void weekRadio_CheckedChanged(object sender, EventArgs e)
         {
-           // calendar = DateTime.Today;
+            calendar = DateTime.Today;
             SetTimeSpan(Date.WEEK);
         }
 
         private void monthRadio_CheckedChanged(object sender, EventArgs e)
         {
-           // calendar = DateTime.Today;
+            calendar = DateTime.Today;
             SetTimeSpan(Date.MONTH);
         }
         /// <summary>
@@ -119,53 +115,43 @@ namespace Dziennik
         /// <param name="today">format dd.mm.yyyy</param>
         /// <param name=""></param>
         /// <returns></returns>
-        private List<Class> SetTimeSpan(Date span)
+        private void SetTimeSpan(Date span)
         {
             timetableListView.Items.Clear();
-            int day = calendar.Day;
-            int month = calendar.Month;
-            String todayDayStr = AddZero(day);
-            String todayMonthStr = AddZero(month) ;
-            int firstDayOfWeek = (int)(calendar.Day - calendar.DayOfWeek);
-            int lastDayOfWeek = (int)(calendar.Day + 6-calendar.DayOfWeek);
-            List<Class> result=new List<Class>();
+            DateTime day = calendar;
+            DateTime firstDayOfWeek = calendar.AddDays(-((int)calendar.DayOfWeek));
+            DateTime lastDayOfWeek = calendar.AddDays(6 - (int)calendar.DayOfWeek);
             for (int i = 0; i < classes.Count; i++)
             {
-                int classDay = Int32.Parse(classes[i].GetDate().Substring(0,2));
+                int classDay = Int32.Parse(classes[i].GetDate().Substring(0, 2));
                 int classMonth = Int32.Parse(classes[i].GetDate().Substring(3, 2));
-                if(span==Date.DAY)
+                int classYear = Int32.Parse(classes[i].GetDate().Substring(6, 4));
+                DateTime classDate = new DateTime(classYear,classMonth,classDay);
+                if (span == Date.DAY)
                 {
-                    if(classMonth == month && classDay==day)
+                    if (classDate == day)
                     {
                         AddToTimeTable(classes[i]);
                     }
                 }
-                else if(span==Date.WEEK)
+                else if (span == Date.WEEK)
                 {
-                    if(classMonth==month && classDay>=firstDayOfWeek && classDay<=lastDayOfWeek)
+                    if(firstDayOfWeek<=classDate && classDate <= lastDayOfWeek)
                     {
                         AddToTimeTable(classes[i]);
                     }
                 }
-                else if(span==Date.MONTH)
+                else if (span == Date.MONTH)
                 {
-                    if (classMonth==month)
+                    if (classDate.Month == day.Month && classDate.Year == day.Year)
                     {
                         AddToTimeTable(classes[i]);
                     }
                 }
             }
+        }
 
-                return result;
-        }
-        private String AddZero(int i)
-        {
-            return i < 10 ? "0" + i.ToString() : i.ToString();
-        }
-        private String AddZero(string str)
-        {
-            return Int32.Parse(str) < 10 ? "0" + str.ToString() : str.ToString();
-        }
+
         private void AddToTimeTable(Class c)
         {
             ListViewItem item = new ListViewItem();
